e0b5c46a99c2519b0d09d630a87b21713dbde3f4
KJane0120 <rlawoals5755@gmail.com>
2025-02-20 12:36:35 +0900

    [Feat]카메라 러프 처리
    러프를 통해 카메라가 부드럽게 움직일 수 있도록 했습니다.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,9 +6,8 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     float offsetY;
-
-    public float minY = 0.0f;
-
+    public float smoothSpeed = 5.0f;
+    public Vector2 minMaxY = new Vector2(0.2f, 19.5f);
     void Start()
     {
         if (target == null)
@@ -22,10 +21,10 @@ public class CameraFollow : MonoBehaviour
         if (target == null)
             return;
 
-        Vector3 pos = transform.position;
-        pos.y = target.position.y + offsetY;
-        pos.y = Mathf.Clamp(pos.y, 0.2f, 19.5f);
+        Vector3 targetPos = transform.position;
+        targetPos.y = target.position.y + offsetY;
+        targetPos.y = Mathf.Clamp(targetPos.y, minMaxY.x, minMaxY.y);
 
-        transform.position = pos;
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothSpeed * Time.deltaTime);
     }
 }
