e84fe6cbee57622f3fa5e7f2627c6b1e5d0486e1
KJane0120 <rlawoals5755@gmail.com>
2025-02-20 15:02:23 +0900

    [Fix]최고점수 출력 버그 수정
    bestScore이 null이 되는 에러 수정했습니다. 정상작동 확인했습니다.

--- Assets/Scripts/TombStone.cs
+++ Assets/Scripts/TombStone.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TombStone : MonoBehaviour
 {
     [SerializeField] private Canvas canvas;
+    [SerializeField] private TextMeshProUGUI bestScoreText;
 
     private void Start()
     {
@@ -13,14 +15,23 @@ public class TombStone : MonoBehaviour
             return;
         else 
             canvas.gameObject.SetActive(false);
+        bestScoreText = GetComponentInChildren<TextMeshProUGUI>(true);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        ShowBest();
         canvas.gameObject.SetActive(true);
+
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         canvas.gameObject.SetActive(false);
+    }
+
+    private void ShowBest()
+    {
+        int BestScore = PlayerPrefs.GetInt("BestScore", 0);
+        bestScoreText.text = BestScore.ToString();
     }
 }
