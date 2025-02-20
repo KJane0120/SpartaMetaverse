8e06027b9d618052a87d663eef1da58857d5b471
KJane0120 <rlawoals5755@gmail.com>
2025-02-20 12:07:03 +0900

    [Fix]카메라 영역 설정
    Mathf.Clamp로 카메라가 특정 영역을 넘어가지 않게 설정했습니다 ..
    //Collision으로 시도했으나 실패

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,17 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
+
 
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     float offsetY;
 
+    public float minY = 0.0f;
+
     void Start()
     {
         if (target == null)
             return;
-
+   
         offsetY = transform.position.y - target.position.y;
     }
 
@@ -22,6 +24,8 @@ public class CameraFollow : MonoBehaviour
 
         Vector3 pos = transform.position;
         pos.y = target.position.y + offsetY;
+        pos.y = Mathf.Clamp(pos.y, 0.2f, 19.5f);
+
         transform.position = pos;
     }
 }
