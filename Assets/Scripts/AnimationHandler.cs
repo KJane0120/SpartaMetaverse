9e00aef158bf60070b885e55933a133fc137ac4a
KJane0120 <rlawoals5755@gmail.com>
2025-02-20 09:49:23 +0900

    [Feat]Player anim 추가
    IsMove 파라미터를 통해 움직일때와 움직이지 않을때의 애니메이션을 구현했습니다.

--- Assets/Scripts/AnimationHandler.cs
+++ Assets/Scripts/AnimationHandler.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class AnimationHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static readonly int IsMoving = Animator.StringToHash("IsMove");
+
+    protected Animator animator;
+
+    protected virtual void Awake()
     {
-        
+        animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector2 obj)
     {
-        
+        animator.SetBool(IsMoving, obj.magnitude > .5f);
     }
+
 }
